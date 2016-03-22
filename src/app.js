07562408e8eb6cf6292fd6bbece24e467383fdaf
Nadjib Amar <hello@ebabel.eu>
2016-03-22 22:56:34 +0100

    working with react toolbox

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Button from 'react-toolbox/lib/button';
+import Navigation from './navigation.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <Navigation />
+        <Button label="Hello world" raised accent />
       </div>
     );
   }
---
+++ src/navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from 'react-toolbox/lib/navigation';
+import Link from 'react-toolbox/lib/link';
+
+const actions = [
+  { label: 'Alarm', raised: true, icon: 'access_alarm'},
+  { label: 'Location', raised: true, accent: true, icon: 'room'}
+];
+
+const NavigationTest = () => (
+  <div>
+    <Navigation type='horizontal' actions={actions} />
+    <Navigation type='vertical'>
+      <Link href='http://' label='Inbox' icon='inbox' />
+      <Link href='http://' active label='Profile' icon='person' />
+    </Navigation>
+  </div>
+);
+
+export default NavigationTest;
