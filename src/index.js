78f9911760572ba4ca7a4cab2fb7bf833bcbe5f1
Nadjib Amar <hello@ebabel.eu>
2016-03-22 22:44:07 +0100

    starting to use react toolbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // Third party dependencies.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ToolboxApp from 'react-toolbox/lib/app';
+
+// Internal dependencies.
 import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ToolboxApp>
+        <App />
+    </ToolboxApp>
+    , document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,43 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        'bundle': './src/index.js',
-        'bundle.min': './src/index.js'
-    },
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    presets: ['react']
-                }
-            },
-            {
-                test: /\.css$/, 
-                loader: 'style!css'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
-            minimize: true,
-            compress: {
-                warnings: false
-            }
-        })
+  entry: {
+    'bundle': './src/index.js',
+    'bundle.min': './src/index.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 };
