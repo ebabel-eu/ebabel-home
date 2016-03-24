09a70fdc257932171dd00432ae37cee6442c003a
nadjibamar <hello@ebabel.eu>
2016-03-24 06:20:52 +0100

    refactoring

--- src/components/navigation-bar/header/header.js
+++ src/components/navigation-bar/header/header.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Header = () => (
-  <div className='navbar-header'>
-    <button type='button' className='navbar-toggle collapsed' 
-        data-toggle='collapse' data-target='#bs-navbar' aria-expanded='false'>
-      <span className='sr-only'>Toggle navigation</span>
-      <span className='icon-bar'></span>
-      <span className='icon-bar'></span>
-      <span className='icon-bar'></span>
-    </button>
-    <span className='navbar-brand'>Nadjib</span>
-  </div>
-);
+class Header extends Component {
+  render() {
+    return (
+      <div className='navbar-header'>
+        <button type='button' className='navbar-toggle collapsed' 
+            data-toggle='collapse' data-target='#bs-navbar' aria-expanded='false'>
+          <span className='sr-only'>Toggle navigation</span>
+          <span className='icon-bar'></span>
+          <span className='icon-bar'></span>
+          <span className='icon-bar'></span>
+        </button>
+        <span className='navbar-brand'>Nadjib</span>
+      </div>
+    )
+  }
+}
 
 export default Header;
