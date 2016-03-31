ffc015a6dd8d1b40c6bd5a9aa7e5b3247d5f66a0
Nadjib Amar <nadjib.amar@efocus.nl>
2016-03-31 16:36:24 +0200

    refactoring

--- src/components/nav/update-page-title.js
+++ src/components/nav/update-page-title.js
@@ -1,12 +1,8 @@
 class UpdatePageTitle {
   constructor(input = { currentUrl: null, links: [] }) {
-    const currentUrl = input.currentUrl;
-    const links = input.links;
-    const currentLink = links.find(link => {
-      if (link.url === currentUrl) {
-        return link;
-      }
-    });
+    const { currentUrl, links } = input;
+
+    const currentLink = links.find(link => link.url === currentUrl);
 
     window.document.title = currentLink.label;
   }
